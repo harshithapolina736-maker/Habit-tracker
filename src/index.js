b645101c94dba3e88ea96099c72b9ae69706b21e
Harshitha <harshithapolina736@gmail.com>
2025-10-25 14:14:08 +0530

    removed_unwanted

--- src/index.js
+++ src/index.js
@@ -8,19 +8,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	// <React.StrictMode>
-	<BrowserRouter
-		future={{
-			v7_startTransition: true,
-			v7_relativeSplatPath: true
-		}}
-	>
+	<React.StrictMode>
+	<BrowserRouter>
 		<App />
 	</BrowserRouter>
-	// </React.StrictMode>
+	</React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
+	
